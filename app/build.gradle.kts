17aa4d3fce5f6230e75352929f6f0e215148be05
Jolie Zhu <zhujolie973@gmail.com>
2026-07-07 07:56:04 -0400

    feat(setup): configured Firebase database and tested a response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(platform(libs.firebase.bom))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/cscb07project/MainActivity.java
+++ app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -18,15 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = FirebaseDatabase.getInstance("https://b07-demo-summer-2024-default-rtdb.firebaseio.com/");
-        DatabaseReference myRef = db.getReference("testDemo");
+        db = FirebaseDatabase.getInstance("https://cscb07-project-e0581-default-rtdb.firebaseio.com/");
+        DatabaseReference myRef = db.getReference("firebaseTest");
+
+        // a test
+        myRef.setValue("Hello, Firebase!");
 
 //        myRef.setValue("B07 Demo!");
-        myRef.child("movies").setValue("B07 Demo!");
-
-        if (savedInstanceState == null) {
-            loadFragment(new HomeFragment());
-        }
+//        myRef.child("movies").setValue("B07 Demo!");
+//
+//        if (savedInstanceState == null) {
+//            loadFragment(new HomeFragment());
+//        }
     }
 
     private void loadFragment(Fragment fragment) {
