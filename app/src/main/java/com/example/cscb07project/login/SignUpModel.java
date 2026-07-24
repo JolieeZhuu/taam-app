bec940a179d7155bc3d427c5752ada2c4e2810e3
Yile Yu <yileyu2@gmail.com>
2026-07-23 21:36:49 -0400

    feat: add proper error messages in singup screen

--- app/src/main/java/com/example/cscb07project/login/SignUpModel.java
+++ app/src/main/java/com/example/cscb07project/login/SignUpModel.java
@@ -2,7 +2,13 @@ package com.example.cscb07project.login;
 
 import com.example.cscb07project.repositories.UserRepository;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class SignUpModel implements MVPInterface.model {
     private final UserRepository userRepo;
@@ -20,7 +26,27 @@ public class SignUpModel implements MVPInterface.model {
     public void authenticateUser(String email, String password, String username, callback callback) {
         userRepo.createUser(username, email, password)
                 .addOnSuccessListener(callback::onSuccess)
-                .addOnFailureListener(e -> callback.onError("PLACEHOLDER ERROR MESSAGE"));
-        //TODO: return custom error messages
+                .addOnFailureListener(e -> callback.onError(getError(e)));
+        //MAJOR ISSUE: PEOPLE CAN HAVE SAME USERNAME.
+    }
+
+    private String getError(Exception e) {
+        if (e instanceof FirebaseAuthWeakPasswordException) {
+            return "Your password is too weak.";
+        } else if (e instanceof FirebaseAuthInvalidCredentialsException) {
+            return "The email address entered is invalid.";
+        } else if (e instanceof FirebaseAuthUserCollisionException) {
+            return "A user with that email already exists.";
+        } else if (e instanceof FirebaseAuthException) {
+            String code = ((FirebaseAuthException) e).getErrorCode();
+
+            switch (code) {
+                case "ERROR_TOO_MANY_ATTEMPTS_TRY_LATER":
+                    return "Too many requests.";
+                case "ERROR_OPERATION_NOT_ALLOWED":
+                    return "Signup is not available at the moment.";
+            }
+        }
+        return "Something went wrong.";
     }
 }
