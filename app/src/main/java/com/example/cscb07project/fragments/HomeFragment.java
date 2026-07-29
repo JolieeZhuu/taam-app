fb194a26e1faa568a9770258e3bf2d297a97516b
andynonymous-guy <andynonymous99@gmail.com>
2026-07-28 21:47:24 -0400

    Refactor: buildable state.

--- app/src/main/java/com/example/cscb07project/fragments/HomeFragment.java
+++ app/src/main/java/com/example/cscb07project/fragments/HomeFragment.java
@@ -16,7 +16,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_home_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_homepage, container, false);
         return view;
     }
 
