8ea4a5008e6316345c2c1b0036ae482accd7bc16
andynonymous-guy <andynonymous99@gmail.com>
2026-07-28 21:48:37 -0400

    Refactor: buildable state.

--- app/src/main/java/com/example/cscb07project/repositories/ArtifactRepository.java
+++ app/src/main/java/com/example/cscb07project/repositories/ArtifactRepository.java
@@ -1,6 +1,5 @@
 package com.example.cscb07project.repositories;
 
-import android.provider.ContactsContract;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -21,8 +20,6 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
