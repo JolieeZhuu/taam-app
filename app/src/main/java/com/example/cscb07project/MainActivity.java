dc2a85a1f84bc0334f6de0ef34cc83649fa8a154
Jolie Zhu <zhujolie973@gmail.com>
2026-07-28 20:44:31 -0400

    refactor: move AddArtifactFragment to fragments folder

--- app/src/main/java/com/example/cscb07project/MainActivity.java
+++ app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.cscb07project.R;
+import com.example.cscb07project.fragments.AddArtifactFragment;
 import com.example.cscb07project.fragments.HomepageFragment;
 import com.example.cscb07project.repositories.ArtifactRepository;
 import com.example.cscb07project.systems.FilterState;
@@ -26,15 +26,10 @@ public class MainActivity extends AppCompatActivity {
         db = FirebaseDatabase.getInstance("https://cscb07-project-e0581-default-rtdb.firebaseio.com/");
         aRep = new ArtifactRepository(db);
 
-//        myRef.setValue("B07 Demo!");
-//         myRef.child("movies").setValue("B07 Demo!");
-
-      // this was from artifact form creation
-//         if (savedInstanceState == null) {
-//             loadFragment(new AddArtifactFragment());
         main_filters = new FilterState();
         if (savedInstanceState == null){
-            loadFragment(new HomepageFragment());
+//            loadFragment(new HomepageFragment());
+            loadFragment(new AddArtifactFragment());
         }
     }
 
