4b6241d63c81d1b32064c574e83af46657007538
andynonymous-guy <andynonymous99@gmail.com>
2026-07-28 22:27:55 -0400

    Fix: Pulled main activity accidentally.

--- app/src/main/java/com/example/cscb07project/MainActivity.java
+++ app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -6,20 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.cscb07project.entities.Artifact;
-import com.example.cscb07project.fragments.HomeFragment;
+import com.example.cscb07project.R;
+import com.example.cscb07project.fragments.HomepageFragment;
 import com.example.cscb07project.repositories.ArtifactRepository;
 import com.example.cscb07project.systems.FilterState;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class MainActivity extends AppCompatActivity {
+
     FirebaseDatabase db;
+    FilterState main_filters;
     ArtifactRepository aRep;
-    FilterState mainFilters;
-    Set<Artifact> selectedArtifacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +26,9 @@ public class MainActivity extends AppCompatActivity {
         db = FirebaseDatabase.getInstance("https://cscb07-project-e0581-default-rtdb.firebaseio.com/");
         aRep = new ArtifactRepository(db);
 
-        mainFilters = new FilterState();
-        selectedArtifacts = new HashSet<>();
-
+        main_filters = new FilterState();
         if (savedInstanceState == null){
-            loadFragment(new HomeFragment());
+            loadFragment(new HomepageFragment());
         }
     }
 
@@ -45,8 +40,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public FilterState getMainFS() {
-        return mainFilters;
+        return main_filters;
     }
-    public ArtifactRepository getMainARep() { return aRep; }
-    public void setMainSelection(Set<Artifact> new_selection) {selectedArtifacts = new_selection; }
+    public ArtifactRepository getMainARep() {return aRep; }
 }
