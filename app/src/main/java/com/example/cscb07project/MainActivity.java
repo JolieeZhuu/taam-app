7bc0e37416c8d15ff68c089840e192c437cfd4e8
elinaelooo <elinaelina1111@gmail.com>
2026-07-30 12:53:12 -0400

    feat: add CollectionRepository and UserRepository to MainActivity with getters and ability to test collections

--- app/src/main/java/com/example/cscb07project/MainActivity.java
+++ app/src/main/java/com/example/cscb07project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cscb07project;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -8,11 +9,15 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.cscb07project.entities.Artifact;
 import com.example.cscb07project.fragments.HomeFragment;
+import com.example.cscb07project.fragments.CatalogueFragment;
+import com.example.cscb07project.repositories.CollectionRepository;
+import com.example.cscb07project.repositories.UserRepository;
 import com.example.cscb07project.repositories.ArtifactRepository;
 import com.example.cscb07project.systems.BatchArtifactRetriever;
 import com.example.cscb07project.systems.FilterState;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -23,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase db;
     ArtifactRepository aRep;
+    CollectionRepository cRep;
+    UserRepository uRep;
     FilterState mainFilters;
     Set<Artifact> selectedArtifacts;
 
@@ -33,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
         db = FirebaseDatabase.getInstance("https://cscb07-project-e0581-default-rtdb.firebaseio.com/");
         aRep = new ArtifactRepository(db);
+        cRep = new CollectionRepository(db);
+        uRep = new UserRepository(db, FirebaseAuth.getInstance());
 
         mainFilters = new FilterState();
         selectedArtifacts = new HashSet<>();
@@ -40,6 +49,17 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null){
             loadFragment(new HomeFragment());
         }
+        /*comment out above if and uncomment this to test colelcton
+
+        if (savedInstanceState == null){
+            uRep.signIn("bruh123@gmail.com","password123")
+                    .addOnSuccessListener(user -> {
+                        Toast.makeText(this, user.getUsername(),
+                                Toast.LENGTH_SHORT).show();
+                        loadFragment(CatalogueFragment.withParameters(8));//catalogue
+                        //sepcifically for collections
+            });*/
+
     }
 
     private void loadFragment(Fragment fragment) {
@@ -53,5 +73,7 @@ public class MainActivity extends AppCompatActivity {
         return mainFilters;
     }
     public ArtifactRepository getMainARep() {return aRep; }
+    public CollectionRepository getMainCRep() {return cRep; }
+    public UserRepository getMainURep() {return uRep; }
     public void setMainSelection(Set<Artifact> new_selection) {selectedArtifacts = new_selection; }
 }
